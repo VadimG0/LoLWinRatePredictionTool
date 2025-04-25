987b421bb43b3f5f16046f84b22ec727190879de
Spektra29 <bostonkarboston@gmail.com>
2025-04-25 18:30:04 -0400

    Improved design, added some logics to showcase team winrate and suggested allies

--- src/frontend/src/StatisticsPanel.js
+++ src/frontend/src/StatisticsPanel.js
@@ -1,11 +1,19 @@
+// StatisticsPanel.js
 import * as React from 'react';
-
 import Box from '@mui/material/Box';
+import PanelOne from './PanelOne';
+import PanelTwo from './PanelTwo';
 
 export default function StatisticsPanel() {
-    return (
-        <Box sx={{ backgroundColor: 'white', minWidth: 400, width: 800, height: 600, padding: 4 }}>            
-        </Box>
+  const [showSecondPanel, setShowSecondPanel] = React.useState(false);
 
-    );
+  const togglePanel = () => {
+    setShowSecondPanel(prev => !prev);
+  };
+
+  return (
+    <Box sx={{ backgroundColor: '#1C2526', minWidth: 400, width: 800, height: 600, padding: 4 }}>
+      {showSecondPanel ? <PanelTwo onClick={togglePanel} /> : <PanelOne onClick={togglePanel} />}
+    </Box>
+  );
 }
